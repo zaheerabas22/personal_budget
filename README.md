b9e6206109e3160671776fea780905ad283f1b23
ZAHEER ABBAS <19011519-122@uog.edu.pk>
2024-08-02 10:17:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# personal_budget
+ created a basic "Personal Budgeting" application focusing on UI design with Firebase Firestore integration
---
+++ android/app/src/main/kotlin/com/example/personal_budget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.personal_budget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
